a87d2865da82dc32caa2c9426d3f655f3fc96991
Sanket Nehe <sanketnehe21@gmail.com>
2025-09-09 17:30:28 +0530

    added basic buttons on main screen to navigate task with basic layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }}/>;
+export default function Layout() {
+  return (
+    <Stack screenOptions={{ headerTitleAlign: "center",headerShown: false }}>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="login" options={{ title: "Login" }} />
+      <Stack.Screen name="posts" options={{ title: "Posts" }} />
+      <Stack.Screen name="counter" options={{ title: "Counter" }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import React from "react";
+import { Button, StyleSheet, View } from "react-native";
 
-export default function Index() {
+export default function index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Practical Task</Text>
+    <View style={styles.container}>
+      <Button title="Go to Login Screen" onPress={() => router.push("/Screens/LoginScreen")} />
+      <Button title="Go to API List" onPress={() => router.push("/Screens/PostListScreen")} />
+      <Button title="Go to Counter" onPress={() => router.push("/Screens/CounterScreen")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    gap: 20,
+    padding: 20,
+  },
+});
