551f427369c6fcc9c099243d661bcb4aa30c9789
Sanket Nehe <sanketnehe21@gmail.com>
2025-09-09 16:33:17 +0530

    feat:create files required in task to be performed

---
+++ app/Screens/BasicLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const BasicLayout = () => {
+  return (
+    <View>
+      <Text>BasicLayout</Text>
+    </View>
+  )
+}
+
+export default BasicLayout
+
+const styles = StyleSheet.create({})
---
+++ app/Screens/PostListScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const PostListScreen = () => {
+  return (
+    <View>
+      <Text>PostListScreen</Text>
+    </View>
+  )
+}
+
+export default PostListScreen
+
+const styles = StyleSheet.create({})
