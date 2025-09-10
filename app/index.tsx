2ad76e0e4010bf09461d19b24aebeb8f98449238
Sanket Nehe <sanketnehe21@gmail.com>
2025-09-10 16:33:09 +0530

    chore : initialize new expo project

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,14 @@
-import { router } from "expo-router";
-import React from "react";
-import { Button, StyleSheet, View } from "react-native";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function index() {
+const index = () => {
   return (
-    <View style={styles.container}>
-      <Button title="Go to Login Screen" onPress={() => router.push("/Screens/LoginScreen")} />
-      <Button title="Go to API List" onPress={() => router.push("/Screens/PostListScreen")} />
-      <Button title="Go to Counter" onPress={() => router.push("/Screens/CounterScreen")} />
+    <View>
+      <Text>index</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    gap: 20,
-    padding: 20,
-  },
-});
+export default index
+
+const styles = StyleSheet.create({})
